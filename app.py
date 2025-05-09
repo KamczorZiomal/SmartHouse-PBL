e058528a44a3f5fa8e6e07421bf8c0c64dd609b4
Jacob <bakson772@gmail.com>
2025-05-09 18:49:25 +0200

    Frontend wyświetla realne wartości

--- app.py
+++ app.py
@@ -5,22 +5,41 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sensors.db'
 db.init_app(app)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/api/data')
 def get_data():
     data = SensorData.query.order_by(SensorData.timestamp.desc()).limit(20).all()
-    result = [{
-        'timestamp': entry.timestamp.strftime("%Y-%m-%d %H:%M:%S"),
-        'temperature': entry.temperature,
-        'humidity': entry.humidity,
-        'air_quality': entry.air_quality,
-        'light_percent': entry.light_percent,
-        'lux': entry.lux
-    } for entry in reversed(data)]  # reversed to show oldest first
+    result = []
+    latest = None
+    oldest = None
+    if data:
+        latest = {
+            'temperature': data[0].temperature,
+            'humidity': data[0].humidity,
+            'air_quality': data[0].air_quality,
+            'light_percent': data[0].light_percent
+        }
+        oldest = {
+            'temperature': data[-1].temperature,
+            'humidity': data[-1].humidity,
+            'air_quality': data[-1].air_quality,
+            'light_percent': data[-1].light_percent
+        }
+        history = [{
+            'timestamp': entry.timestamp.strftime("%Y-%m-%d %H:%M:%S"),
+            'temperature': entry.temperature,
+            'humidity': entry.humidity,
+            'air_quality': entry.air_quality,
+            'light_percent': entry.light_percent
+        } for entry in reversed(data)]
+        result = {'latest': latest, 'oldest': oldest, 'history': history}
     return jsonify(result)
+
 
 if __name__ == '__main__':
     with app.app_context():
